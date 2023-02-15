4af68f8f450c8f8a210136fc0579c808a930e19f
MariolaH <mariola.hullings@student.awesomeinc.org>
2023-02-15 21:03:47 +0000

    added url to JS

--- main.js
+++ main.js
@@ -1,31 +1,73 @@
 let main = document.getElementById('main');
 
-// 1. create onclick
+// 1. create onclick and input 
+
+
+let button = document.createElement('button')
+button.textContent = 'Get Weather'
+button.addEventListener('click', () =>  {
+    // console.log('aaaaaaaaaaaaaaa');
+getData();
+}
+)
+main.appendChild(button);
+
+let input = document.createElement('input')
+input.textContent = 'input'
+main.appendChild(input)
+
+// getting the zipCode
+
+let zipCode; 
 
 // 2. create object that connect to html
 //     city
 //     temperature
 //         Kelvin, Fahrenheit, and Celsius
 //     condition
-//     other info
+//     Other Info
 //         ie. image
 
 
-        function Items(weather) {
-            let li = document.createElement('li');
-            li.textContent = weather;
-            return li;
-        }
- 
-        const main = document.querySelector('main');
-        main.appendChild(createmainItem('City'));
-        main.appendChild(createmainItem('Temperature'));
-        main.appendChild(createmainItem('Condition'));
-        main.appendChild(createmainItem('Other Info'));
+let city = document.createElement('div')
+city.textContent = 'City'
+main.appendChild(city)
 
+let temperature = document.createElement('div')
+temperature.textContent = 'Temperature'
+main.appendChild(temperature)
+
+let condition = document.createElement('div')
+condition.textContent = 'Condition'
+main.appendChild(condition)
+
+let otherInfo = document.createElement('div')
+otherInfo.textContent = 'Other Info'
+main.appendChild(otherInfo)
 
 // 3. connect API
 
+// let url = `https://api.openweathermap.org/data/2.5/weather?zip=${zipCode},us&appid=6a59100ef22a193081d29e1193fbc24d`
+
+
+// function getData
+// gets data from api
+
+function getData() {
+    zipCode = document.getElementById('input');
+    axios.get(`https://api.openweathermap.org/data/2.5/weather?zip=${zipCode},us&appid=6a59100ef22a193081d29e1193fbc24d`)
+        .then(function (response) {
+            console.log(response);
+            console.log("main", response.data)
+            main = response.data;
+            console.log(main);
+            createListItems(main);
+        })
+        .catch(function (error) {
+            console.log(error);
+        })
+
+}
 
 // 4. create functions that converts Kelvin, Fahrenheit, and Celsius
 
