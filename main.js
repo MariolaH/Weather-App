97087e671ea82101e6fcda8df9c8fce167921c6d
MariolaH <mariola.hullings@student.awesomeinc.org>
2023-02-16 16:07:24 +0000

    changes

--- main.js
+++ main.js
@@ -12,9 +12,18 @@ getData();
 )
 main.appendChild(button);
 
+ 
+
 let input = document.createElement('input')
 input.textContent = 'input'
 main.appendChild(input)
+
+
+// rest button
+
+addEventListener('rest',(event) => {});
+onset = (event) => {};
+
 
 // getting the zipCode
 
@@ -32,51 +41,69 @@ let mainData = [];
 
 function display() {
 
-   
+// City section   
 
 let city = document.createElement('div')
 // city.classList.add(container, center) 
 city.textContent = 'City'
 main.appendChild(city)
 
-let cityValue = document.createElement('div')
-cityValue.textContent = mainData.name;
-main.appendChild(cityValue)
+// gets data from API
+
+// let cityValue = document.createElement('div')
+// cityValue.textContent = mainData.name;
+// main.appendChild(cityValue)
+
+// Temp section & convet k to c, K to F
+// This renders kelvin
 
 let temperature = document.createElement('div')
 temperature.textContent = 'Temperature'
 main.appendChild(temperature)
 
+// gets data from API
+
 let tempValue = document.createElement('div')
 tempValue.textContent = mainData.main.temp
 main.appendChild(tempValue)
+
+// convet k to c
+
+// function kelvinToCelsius(celsius) {
+//     return kelvin - 273.15;
+}
+
+// convet k to F
+
+// function kelvinToFahrenheit(fahrenheit)
+//     return kelvin * (9/5) - 459.67;
+
+// conditions section
 
 let condition = document.createElement('div')
 condition.textContent = 'Condition'
 main.appendChild(condition)
 
-let conditionValue = document.createElement('div')
-conditionValue.textContent = mainData.main.clouds
-main.appendChild(conditionValue)
+// gets data from API
+
+// let conditionValue = document.createElement('div')
+// conditionValue.textContent = mainData.main.clouds
+// main.appendChild(conditionValue)
+
+// other info ie.  image
 
 let otherInfo = document.createElement('div')
 otherInfo.textContent = 'Other Info'
 main.appendChild(otherInfo)
-}
 
 // 3. connect API
-
-// let url = `https://api.openweathermap.org/data/2.5/weather?zip=${zipCode},us&appid=6a59100ef22a193081d29e1193fbc24d`
-
-
-// function getData
-// gets data from api
 
 function getData() {
     // zipCode = document.getElementById('input').value;
     console.log(zipCode);
     axios.get(`https://api.openweathermap.org/data/2.5/weather?zip=${zipCode},us&appid=6a59100ef22a193081d29e1193fbc24d`)
         .then(function (response) {
+            // handle sucess
             console.log(response);
             console.log("main", response.data)
             mainData = response.data;
@@ -94,6 +121,11 @@ function getData() {
 
 // 4. create functions that converts Kelvin, Fahrenheit, and Celsius
 
+
+
+
+
+// created case for each state on page
 
 
 
